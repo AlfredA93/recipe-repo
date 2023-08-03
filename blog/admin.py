44932dcc15bc5ab2248778ddae44a4d1c526368e
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-03 10:57:41 +0200

    Register Recipe model in admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Recipe
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    "Recipe Admin fields and filtering"
+    list_filter = ('status', 'season', 'published_on')
+    list_display = ('title', 'status', 'season', 'published_on')
+    search_fields = ['title', 'instructions']
