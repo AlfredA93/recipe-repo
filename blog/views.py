8e2aa0e388590b216d33ef4e84f5d7b1f773aa0a
Alfred Ayre <alfred.w.ayre@gmail.com>
2023-08-08 10:32:46 +0200

    Create RecipeDetail view in views.py

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.views import generic
+from django.shortcuts import render, get_object_or_404
+from django.views import generic, View
 from .models import Recipe
 
 
@@ -8,3 +8,24 @@ class RecipeList(generic.ListView):
     queryset = Recipe.objects.filter(status=1).order_by('-likes')
     template_name = 'index.html'
     paginate_by = 9
+
+
+class RecipeDetail(View):
+
+    def get(self, request, slug, *args, **kwargs):
+        queryset = Recipe.objects.filter(status=1, slug=slug)
+        recipe = get_object_or_404(queryset, slug=slug)
+        comments = recipe.comments.order_by('published')
+        liked = False
+        if recipe.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
+        return render(
+            request,
+            "recipe_detail.html",
+            {
+                "recipe": recipe,
+                "comments": comments,
+                "liked": liked
+            },
+        )
